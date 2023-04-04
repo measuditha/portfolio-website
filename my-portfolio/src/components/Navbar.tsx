a67afab31ee1605acb60567fcdfcec363f34fec5
measuditha <chathurangaadhikary@gmail.com>
2023-04-04 20:15:35 +0530

    1st

---
+++ my-portfolio/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+
+
+
+export default function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
