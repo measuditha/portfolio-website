6256b359dfd6954034d7788e6d878cb2580d167d
measuditha <chathurangaadhikary@gmail.com>
2023-04-05 09:54:44 +0530

    2

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={< Home />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
