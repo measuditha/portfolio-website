97a821a77307827406d3961ced943acd7ebbf794
measuditha <chathurangaadhikary@gmail.com>
2023-04-21 22:48:00 +0530

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import './App.css'
 import { BrowserRouter, Routes, Route , Link } from 'react-router-dom'
 import Home from "./pages/Home"
 import AboutMe from './pages/AboutMe'
-import ClentReviews from './pages/ClentReviews'
+import ClientReviews from './pages/ClientReviews'
 import ContactMe from './pages/ContactMe'
 import Error from './pages/Error'
 import Faq from './pages/Faq'
@@ -12,23 +11,27 @@ import Resume from './pages/Resume'
 import SocialMedia from './pages/SocialMedia'
 import WhatIdo from './pages/WhatIdo'
 
+
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={< Home />}/>
-        <Route path='/about' element={< AboutMe />}/>
-        <Route path='/review' element={< ClentReviews />}/>
-        <Route path='/contact' element={< ContactMe />}/>
-        <Route path='/faq' element={< Faq />}/>
-        <Route path='/portfolio' element={< Portfolio />}/>
-        <Route path='/resume' element={< Resume />}/>
-        <Route path='/social' element={< SocialMedia/>}/>
-        <Route path='/me' element={< WhatIdo />}/>
-        <Route path='/error' element={< Error />}/>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={< Home />}/>
+    //     <Route path='/about' element={< AboutMe />}/>
+    //     <Route path='/review' element={< ClientReviews />}/>
+    //     <Route path='/contact' element={< ContactMe />}/>
+    //     <Route path='/faq' element={< Faq />}/>
+    //     <Route path='/portfolio' element={< Portfolio />}/>
+    //     <Route path='/resume' element={< Resume />}/>
+    //     <Route path='/social' element={< SocialMedia/>}/>
+    //     <Route path='/me' element={< WhatIdo />}/>
+    //     <Route path='*' element={< Error />}/>
+    //   </Routes>
+    // </BrowserRouter>
+    <div className="text-3xl font-bold underline">
+      TEST
+    </div>
   )
 }
 
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Home from './Home'
 
 export default function Error() {
   return (
-    <div>Error</div>
+    <div className="d-flex align-items-center justify-content-center vh-100">
+      <div className="text-center">
+        <h1 className="display-1 fw-bold">404</h1>
+        <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
+        <p className="lead">
+          The page you’re looking for doesn’t exist.
+        </p>
+        <a className="btn btn-danger">Go Home</a>
+      </div>
+    </div>
   )
 }
