03842d7432a94f1ff5b35996199d532beb3a6c2e
measuditha <chathurangaadhikary@gmail.com>
2023-04-23 19:40:32 +0530

    Navbar Trancitions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,40 +10,40 @@ export default function Navbar() {
       <div>
         <span className="text-4xl font-bold"> UDITHA </span>
       </div>
-      <div className='flex items-center justify-end space-x-4'>
-        <ul className='flex items-center justify-end space-x-6'>
-          <li>
+      <div className='flex items-center justify-end space-x-4 '>
+        <ul className='flex items-center justify-end space-x-6 font-bold'>
+          <li className='hover:underline underline-offset-8 decoration-2  decoration-orange-500'>
             <Link to="/">HOME</Link>
           </li>
-          <li>
+          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
             <Link to="/about">ABOUT</Link>
           </li>
-          <li>
+          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
             <Link to="/review">CLIENTS</Link>
           </li>
-          <li>
+          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
             <Link to='/contact'>CONTACT</Link>
           </li>
-          <li>
+          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
             <Link to='/faq'>FAQ</Link>
           </li>
-          <li>
+          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
             <Link to='/portfolio'>PORTFOLIO</Link>
           </li>
-          <li>
+          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
             <Link to='/resume'>RESUME</Link>
           </li>
-          <li>
+          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
             <Link to='/me'>SERVICES</Link>
           </li>
           <Link to='*' />
         </ul>
       </div>
-      <div className='flex items-center justify-end space-x-4'>
-        <span className='text-xl'>
+      <div className='flex items-center justify-end space-x-4 p-10'>
+        <span className='text-xl hover:scale-125 transition duration-300'>
           <BsLinkedin />
         </span>
-        <span className='text-xl'>
+        <span className='text-xl hover:scale-125 transition duration-300'>
           <BsGithub />
         </span>
       </div>
