f314e47168873081d4494b571b7b36d198729027
measuditha <chathurangaadhikary@gmail.com>
2023-04-26 11:46:36 +0530

    navbar hover effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,28 +12,28 @@ export default function Navbar() {
       </div>
       <div className='flex items-center justify-end space-x-4 '>
         <ul className='flex items-center justify-end space-x-6 font-bold'>
-          <li className='hover:underline underline-offset-8 decoration-2  decoration-orange-500'>
+          <li className='relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-[2.5px] before:rounded-full before:opacity-0 before:transition-all before:duration-300 before:bg-orange-500 hover:before:w-full hover:before:opacity-100'>
             <Link to="/">HOME</Link>
           </li>
-          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
+          <li className='relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-[2.5px] before:rounded-full before:opacity-0 before:transition-all before:duration-300 before:bg-orange-500 hover:before:w-full hover:before:opacity-100'>
             <Link to="/about">ABOUT</Link>
           </li>
-          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
+          <li className='relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-[2.5px] before:rounded-full before:opacity-0 before:transition-all before:duration-300 before:bg-orange-500 hover:before:w-full hover:before:opacity-100'>
             <Link to="/review">CLIENTS</Link>
           </li>
-          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
+          <li className='relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-[2.5px] before:rounded-full before:opacity-0 before:transition-all before:duration-300 before:bg-orange-500 hover:before:w-full hover:before:opacity-100'>
             <Link to='/contact'>CONTACT</Link>
           </li>
-          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
+          <li className='relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-[2.5px] before:rounded-full before:opacity-0 before:transition-all before:duration-300 before:bg-orange-500 hover:before:w-full hover:before:opacity-100'>
             <Link to='/faq'>FAQ</Link>
           </li>
-          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
+          <li className='relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-[2.5px] before:rounded-full before:opacity-0 before:transition-all before:duration-300 before:bg-orange-500 hover:before:w-full hover:before:opacity-100'>
             <Link to='/portfolio'>PORTFOLIO</Link>
           </li>
-          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
+          <li className='relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-[2.5px] before:rounded-full before:opacity-0 before:transition-all before:duration-300 before:bg-orange-500 hover:before:w-full hover:before:opacity-100'>
             <Link to='/resume'>RESUME</Link>
           </li>
-          <li className='hover:underline underline-offset-8 decoration-2 decoration-orange-500'>
+          <li className='relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-[2.5px] before:rounded-full before:opacity-0 before:transition-all before:duration-300 before:bg-orange-500 hover:before:w-full hover:before:opacity-100'>
             <Link to='/me'>SERVICES</Link>
           </li>
           <Link to='*' />
